16c9ac7b0e5c012f721fe8504ae4501db7f76afa
Will Koury <wgkoury@gmail.com>
2020-08-18 19:40:40 -0500

    Minimum viable product

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase";
+require("dotenv").config();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "covid-19-over-under.firebaseapp.com",
+  databaseURL: "https://covid-19-over-under.firebaseio.com",
+  projectId: "covid-19-over-under",
+  storageBucket: "covid-19-over-under.appspot.com",
+  messagingSenderId: "406424898192",
+  appId: "1:406424898192:web:930aa112756a8289f1a615"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 8
+    };
+  }
+
+  handleChange = event => {
+    event.preventDefault();
+    this.setState({
+      value: event.target.value
+    });
+  }
+
+  getVotes = async () => {
+    const votesRef = db.collection("votes");
+    const snapshot = await votesRef.get();
+
+    let sum = 0;
+    let denom = 0;
+
+    snapshot.forEach(doc => {
+      sum += doc.data().weeks;
+      denom++;
+    });
+
+    let average = sum / denom;
+    this.setState({ average: average });
+  }
+
+  handleClick = async event => {
+    event.preventDefault();
+
+    await db.collection("votes").add({
+      weeks: this.state.value
+    });
+
+    this.setState({
+      value: 8
+    });
+    
+    localStorage.setItem("voted", "yes");
+  }
+
+  componentDidMount() {
+    if(localStorage.getItem("voted") === "yes"){
+      this.setState({ voted: true });
+    }else{
+      this.setState({ voted: false });
+    }
+
+    this.getVotes();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="average">
+          <h3>So far, people think that we will make it {this.state.average} weeks before schools get shutdown.</h3>
+          <h4>Submit your own estimate:</h4>
+        </div>
+        {!this.state.voted && (
+          <div className="input">
+            <input
+              type="range"
+              min={0}
+              max={16}
+              value={this.state.value}
+              onChange={e => this.handleChange(e)}
+              />
+            <button
+              onClick={e => this.handleClick(e)}
+            >
+              Submit
+            </button>
+            <p>{this.state.value} weeks</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
